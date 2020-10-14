57b91e6760d6418b82236d2cf61b6a1cd0e48f3e
strangenar <narmirz2@gmail.com>
2020-10-14 18:39:41 +0600

    added LogInPage

--- hotelmanagement/RegistrationPage.java
+++ hotelmanagement/RegistrationPage.java
@@ -3,10 +3,7 @@ package hotelmanagement;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 import com.google.gson.Gson;
