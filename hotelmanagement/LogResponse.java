dcffac24a295d7bc1c76f0534ad426c19561c32b
strangenar <narmirz2@gmail.com>
2020-10-15 11:05:23 +0600

    Slight changes login

---
+++ hotelmanagement/LogResponse.java
@@ -0,0 +1,9 @@
+package hotelmanagement;
+
+public class LogResponse {
+    private String message;
+
+    public void setMessage(String msg){ this.message = msg; }
+    public String getMessage(){ return this.message; }
+}
+
